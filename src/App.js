12b08bc6ed356db2d0e7a2a8f5b7d800bde441bf
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2021-05-15 15:41:07 -0300

    feat: add global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-function App() {
-  return <h1>Hello</h1>
+import GlobalStyles from './styles/global'
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello</h1>
+    </>
+  )
 }
 
 export default App
