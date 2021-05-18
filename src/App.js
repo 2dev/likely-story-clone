37aa367efebcad547916b69fc37ff8390f96246e
Fernando Rodrigues <72588972+devfernandorodrigues@users.noreply.github.com>
2021-05-17 21:52:42 -0300

    feat: add theme configuration (#6)
    * feat: add theme configuration
    
    * feat: add ThemeProvider
    
    * refactor: remove unnecessary fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <h1>Hello</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,48 @@
+export default {
+  grid: {
+    container: '130rem',
+    gutter: '3.2rem',
+  },
+  font: {
+    family:
+      "Montserrat, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    extraBold: 700,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '6.4rem',
+      huge: '14rem',
+    },
+  },
+  colors: {
+    primary: '#fc589f',
+    secondary: '#5214ff',
+    yellowBg: '#fce03d',
+    orangeBg: '#ffbc42',
+    pinkLightBg: '#f7d4d5',
+    darkBg: '#141221',
+    white: '#FFFFFF',
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem',
+  },
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50,
+  },
+}
