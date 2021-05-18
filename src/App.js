b9667d92e1c657b4cfb79a9853a036ab1ff698ed
Fernando Rodrigues <devfernandorodrigues@gmail.com>
2021-05-17 21:42:49 -0300

    feat: add ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from './styles/global'
+import theme from './styles/theme'
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <h1>Hello</h1>
+      <ThemeProvider theme={theme}>
+        <h1>Hello</h1>
+      </ThemeProvider>
     </>
   )
 }
