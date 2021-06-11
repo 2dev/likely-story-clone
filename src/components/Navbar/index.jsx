57faa20843bbebfd8185f1371a8896dc98d9d5aa
Mateus Victor <mateus.svictor@gmail.com>
2021-06-11 19:23:58 -0300

    feat: add Navbar component

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+import Logo from '../Logo'
+import NavbarButton from '../NavbarButton'
+
+const Navbar = () => {
+  return (
+    <S.Wrapper>
+      <Logo />
+      <NavbarButton />
+    </S.Wrapper>
+  )
+}
+
+export default Navbar
