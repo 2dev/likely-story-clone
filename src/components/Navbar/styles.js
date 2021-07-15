8e38dabd43032d3835b868747fbee669221201a0
Mateus Victor <mateus.svictor@gmail.com>
2021-07-15 19:48:28 -0300

    fix: add container to fix navbar position (#18)

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,16 +1,22 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.nav`
+export const Container = styled.nav`
+  position: fixed;
+  width: 100%;
+`
+
+export const Wrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
     justify-content: space-between;
     align-items: center;
 
-    margin-top: ${theme.spacings.large};
     max-height: 10rem;
-    width: 100%;
+    max-width: ${theme.grid.container};
 
-    position: fixed;
     z-index: ${theme.layers.menu};
+
+    margin: 0 auto;
+    padding: 8rem;
   `}
 `
