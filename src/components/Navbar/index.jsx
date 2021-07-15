d990753ec371aacf001c66641121269840183280
Mateus Victor <mateus.svictor@gmail.com>
2021-07-15 19:45:11 -0300

    fix: add container to fix navbar position

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,10 +5,12 @@ import NavbarButton from '../NavbarButton'
 
 const Navbar = () => {
   return (
-    <S.Wrapper>
-      <Logo />
-      <NavbarButton />
-    </S.Wrapper>
+    <S.Container>
+      <S.Wrapper>
+        <Logo />
+        <NavbarButton />
+      </S.Wrapper>
+    </S.Container>
   )
 }
 
