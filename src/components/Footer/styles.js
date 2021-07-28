0b8fd0a697f77282879057df0a3d721c59322c6c
Mateus Victor <mateus.svictor@gmail.com>
2021-07-27 21:13:29 -0300

    feat: add Footer component

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+// import SocialMedia from '../SocialMedia'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const FooterItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  height: 7.2rem;
+`
+
+export const SocialMediaWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 4rem;
+`
