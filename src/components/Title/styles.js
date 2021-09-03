da709dd71c59d4264cd7501073e7b9820f81876b
Mateus Victor <mateus.svictor@gmail.com>
2021-09-02 21:16:37 -0300

    refactor: remove unused modifiers (#40)

--- src/components/Title/styles.js
+++ src/components/Title/styles.js
@@ -1,20 +1,9 @@
 import styled, { css } from 'styled-components'
 
-const titleModifiers = {
-  huge: theme => css`
-    font-size: ${theme.font.sizes.huge};
-  `,
-
-  xxlarge: theme => css`
-    font-size: ${theme.font.sizes.xxlarge};
-  `,
-}
-
 export const Title = styled.h1`
   ${({ theme, size, color }) => css`
     font-weight: ${theme.font.extraBold};
+    font-size: ${theme.font.sizes[size]};
     color: ${theme.colors[color]};
-
-    ${!!size && titleModifiers[size](theme)}
   `}
 `
